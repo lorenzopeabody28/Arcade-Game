d383a4a93e872867f3a4faf387b12b180c6eec7c
lorenzopeabody28 <lorenzo_peabody28@milton.edu>
2026-04-08 09:05:45 -0400

    Inclass 4/8

--- src/BearG.java
+++ src/BearG.java
@@ -49,8 +49,7 @@ public class BearG {
 
     //The move method.  Everytime this is run (or "called") the hero's x position and y position change by dx and dy
     public void move() {
-        xpos = xpos + dx;
-        ypos = ypos + dy;
+
 
         if(isNorth == true && isSouth == false) {
             dy = -4;
@@ -90,7 +89,11 @@ public class BearG {
             dy = 0;
         }
 
+        xpos = xpos + dx;
+        ypos = ypos + dy;
+
         hitbox = new Rectangle(xpos, ypos, width, height);
+
 
 
         //todo: bounce off all walls
